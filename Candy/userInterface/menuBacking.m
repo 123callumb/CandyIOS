e8b378d579a20933b334c85f83ca14eb4c4bf5c0
Callum Beckwith <CallumBeckwith_@pc-203-89.customer.charlotte.hallam.lan>
2016-11-15 20:28:30 +0000

    Better?

--- Candy/userInterface/menuBacking.m
+++ Candy/userInterface/menuBacking.m
@@ -24,7 +24,7 @@
     return sBack;
 }
 +(id)createUpgradesBacking {
-    SKSpriteNode *upgradeMenu = [SKSpriteNode spriteNodeWithImageNamed:@"UpgradeMenu3"];
+    SKSpriteNode *upgradeMenu = [SKSpriteNode spriteNodeWithImageNamed:@"menuUpgrades"];
     upgradeMenu.xScale = 0.43;
     upgradeMenu.yScale = 0.43;
     upgradeMenu.anchorPoint = CGPointMake(0.5, 0.55);
