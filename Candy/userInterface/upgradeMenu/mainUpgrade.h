2f4fa38225f8f2ccfb3d1201eab98d805a39bfc2
Callum Beckwith <CallumBeckwith_@pc-203-89.customer.charlotte.hallam.lan>
2016-11-15 20:07:43 +0000

    With the upgrade template

---
+++ Candy/userInterface/upgradeMenu/mainUpgrade.h
@@ -0,0 +1,13 @@
+//
+//  mainUpgrade.h
+//  Candy
+//
+//  Created by Callum Beckwith on 15/11/2016.
+//  Copyright © 2016 Kilcal. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface mainUpgrade : SKSpriteNode
+
+@end
---
+++ Candy/userInterface/upgradeMenu/mainUpgrade.m
@@ -0,0 +1,15 @@
+//
+//  mainUpgrade.m
+//  Candy
+//
+//  Created by Callum Beckwith on 15/11/2016.
+//  Copyright © 2016 Kilcal. All rights reserved.
+//
+
+#import "mainUpgrade.h"
+
+@implementation mainUpgrade
++(void)addUpgradePages: (SKSpriteNode*)s {
+    
+}
+@end
