b9d4fc6db6f075e743bdbffc8b69260e1f52a48c
Killian Comerford <killian@Killians-MacBook-Pro.local>
2016-11-30 14:20:27 +0000

    Pushing Changes

--- Character.m
+++ Character.m
@@ -9,7 +9,7 @@
 #import "Character.h"
 
 @implementation Character
-+(void)Spawn:(SKScene*)s{
++(void)Spawn:(SKScene*)s Position:(CGPoint)Pos{
     NSString *HatName = [[NSUserDefaults standardUserDefaults] stringForKey:@"hatsVal"];
     NSString *PantsName = [[NSUserDefaults standardUserDefaults] stringForKey:@"pantsVal"];
     NSString *ShirtName = [[NSUserDefaults standardUserDefaults] stringForKey:@"shirtsVal"];
@@ -28,9 +28,18 @@
     //Hat Shadow
     SKSpriteNode *HatShadow = [SKSpriteNode spriteNodeWithImageNamed:HatName];
     HatShadow.color = [SKColor blackColor];
-    HatShadow.position = CGPointMake(10, -10);
     HatShadow.alpha = 0.1;
     HatShadow.colorBlendFactor = 1;
+    
+    base.position = Pos;
+    hue.position = Pos;
+    hat.position = Pos;
+    shirt.position = Pos;
+    pants.position = Pos;
+    shoes.position = Pos;
+    preset.position = Pos;
+    shadow.position = Pos;
+    HatShadow.position = CGPointMake(Pos.x+10, Pos.y-10);
     
     base.zPosition = -2;
     hue.zPosition = -2;
@@ -48,7 +57,7 @@
     long isPre = [[NSUserDefaults standardUserDefaults] integerForKey:@"isPreset"];
     
     [s addChild:shadow];
-    [s addChild:HatShadow];
+    if(isPre == 0)[s addChild:HatShadow];
     [s addChild:base];
     [s addChild:hue];
     if(isPre == 0){
