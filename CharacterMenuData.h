e583f05c26a7f317cf315e98e461b3a14edb8b2b
Killian Comerford <killian@Killians-MacBook-Pro.local>
2016-12-01 17:25:23 +0000

    Changes the Character Creator so that it only opens for the first launch

---
+++ CharacterMenuData.h
@@ -0,0 +1,13 @@
+//
+//  CharacterMenuData.h
+//  Candy
+//
+//  Created by Killian Comerford on 01/12/2016.
+//  Copyright © 2016 Kilcal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CharacterMenuData : NSUserDefaults
+
+@end
