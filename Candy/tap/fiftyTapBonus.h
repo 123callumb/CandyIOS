953e4659fd703506aff3b28afb1141315679c9a9
Callum Beckwith <123callumb@gmail.com>
2016-12-06 23:33:52 +0000

    banter

---
+++ Candy/tap/fiftyTapBonus.h
@@ -0,0 +1,13 @@
+//
+//  fiftyTapBonus.h
+//  Candy
+//
+//  Created by Callum Beckwith on 06/12/2016.
+//  Copyright © 2016 Kilcal. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface fiftyTapBonus : SKSpriteNode
+
+@end
---
+++ Candy/tap/fiftyTapBonus.m
@@ -0,0 +1,12 @@
+//
+//  fiftyTapBonus.m
+//  Candy
+//
+//  Created by Callum Beckwith on 06/12/2016.
+//  Copyright © 2016 Kilcal. All rights reserved.
+//
+
+#import "fiftyTapBonus.h"
+
+@implementation fiftyTapBonus
+@end
