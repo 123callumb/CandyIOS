3574b021a9794dd539205b3881ee8f00ad6effb4
Callum Beckwith <123callumb@gmail.com>
2017-01-30 16:59:37 +0000

    Firebase Framework installed!
    okay progress!

--- Candy/AppDelegate.m
+++ Candy/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Firebase.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [FIRApp configure];
     // Override point for customization after application launch.
     return YES;
 }
